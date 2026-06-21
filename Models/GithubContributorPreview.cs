485559950242e1505e23b9aad0bafd996e24d0eb
YoannDev90 <yoanndev@outlook.fr>
2026-06-21 17:48:14 +0200

    Update GithubContributorPreview (IDisposable, INotifyPropertyChanged)

--- Models/GithubContributorPreview.cs
+++ Models/GithubContributorPreview.cs
@@ -1,26 +1,51 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Avalonia.Media.Imaging;
 
 #pragma warning disable 300
 
 namespace Github_Trend;
 
-public sealed class GithubContributorPreview
+public sealed class GithubContributorPreview : IDisposable, INotifyPropertyChanged
 {
+    private Bitmap? _avatarImage;
+
     public GithubContributorPreview(string login, string? avatarUrl, Bitmap? avatarImage)
     {
         Login = login;
         AvatarUrl = avatarUrl;
-        AvatarImage = avatarImage;
-        _ = Login;
-        _ = AvatarUrl;
-        _ = AvatarImage;
+        _avatarImage = avatarImage;
     }
 
     public string Login { get; }
 
     public string? AvatarUrl { get; }
 
-    public Bitmap? AvatarImage { get; }
+    public Bitmap? AvatarImage
+    {
+        get => _avatarImage;
+        set
+        {
+            if (ReferenceEquals(_avatarImage, value)) return;
+            var old = _avatarImage;
+            _avatarImage = value;
+            OnPropertyChanged();
+            old?.Dispose();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public void Dispose()
+    {
+        _avatarImage?.Dispose();
+    }
 }
 
 #pragma warning restore 300
